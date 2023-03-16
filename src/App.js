39a9725d97eda092d669562896374a180c7edcf7
Aditya Raj <101452242+aditya8Raj@users.noreply.github.com>
2023-03-16 11:49:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+  state = { advice: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        this.setState({ advice: response.data.slip.advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">
+            {advice}
+          </h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>
+              Give me Advice!
+            </span>
+          </button>
+        </div>
+      </div>
+      </>
+    )
+  }
+}
+
+export default App;
+
+
+//------------------------------
+
+// class App extends React.Component {
+//   state = { advice: "" };
+
+//   componentDidMount() {
+//     this.fetchAdvice();
+//   }
+
+//   fetchAdvice = () => {
+//     axios
+//       .get("https://api.adviceslip.com/advice")
+//       .then((response) => {
+//         this.setState({ advice: response.data.slip.advice });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   render() {
+//     console.log(this.state.advice);
+//     return <h1>Hello World</h1>;
+//   }
+// }
